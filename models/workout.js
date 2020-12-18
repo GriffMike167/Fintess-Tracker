e519fb9e5371698e58fdb1db83b3051d677ef6a3
Michael Griffith <griffithcmichael@gmail.com>
2020-12-18 16:24:23 -0600

    workout model

---
+++ models/workout.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        tyoe: Date;
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type:Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0
+    }
+});
