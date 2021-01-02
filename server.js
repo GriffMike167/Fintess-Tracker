ac5ab42a1e7262ade6e8e70ae5ea538af7fd7b53
Michael Griffith <72365691+GriffMike167@users.noreply.github.com>
2021-01-02 13:19:13 -0600

    git push not working

--- server.js
+++ server.js
@@ -2,26 +2,24 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.port || 3000
-
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
-    useFindAndModify:false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useFindAndModify:false
 })
 
-require("./routes/api-routes").default;
-require("./routes/html-routes");
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, () => {
-    console.log(`App runnng on port ${PORT}`)
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
 });
