b27058d82f1fd1b9a37732dac9c51fbfc0baf600
Michael Griffith <griffithcmichael@gmail.com>
2021-01-04 15:26:12 -0600

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/fitnesstrscker";
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
-    useFindAndModify:false
+    useFindAndModify:false,
+    useCreateIndex: true,
+       useFindAndModify: false
 })
 
 require("./routes/apiRoutes")(app);
