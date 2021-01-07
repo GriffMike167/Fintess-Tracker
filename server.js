0e981c3acdf476509d68c79f1dabbc6a2b06ddae
Michael Griffith <griffithcmichael@gmail.com>
2021-01-07 13:31:16 -0600

    changes

--- server.js
+++ server.js
@@ -5,29 +5,26 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const Workout = require('./models/workout')
+const Workout = require("./models/workout");
 
 app.use(morgan("dev"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect( 
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-{  
-    useNewUrlParser:true,
-    useFindAndModify:false,
-    useCreateIndex: true,
-    useNewUrlParser: true
-
-})
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-app.listen(PORT,function(){ 
-    console.log(`App listening on Port ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`App listening on Port ${PORT}`);
 });
 
 // Workout.create(Workout)
