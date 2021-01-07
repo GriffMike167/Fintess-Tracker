c0d18b134f0f69e82c22fcf677bda0a65052a45a
Michael Griffith <griffithcmichael@gmail.com>
2021-01-06 19:06:52 -0600

    updates

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema(
+const workoutSchema = new Schema(
     {
       day: {
         type: Date,
@@ -51,6 +51,6 @@ const WorkoutSchema = new Schema(
     }, 0);
   });
   
-  const Workout = mongoose.model("Workout", WorkoutSchema);
+  const Workout = mongoose.model("Workout", workoutSchema);
   
   module.exports = Workout;
